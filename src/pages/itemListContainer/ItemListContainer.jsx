c48b2de9338512822fc4cd011ca9943aa621d911
ctaboada <108905575+tabu86@users.noreply.github.com>
2024-06-13 23:06:24 -0300

    entregable oks

--- src/pages/itemListContainer/ItemListContainer.jsx
+++ src/pages/itemListContainer/ItemListContainer.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const 'ItemListContainer' = ({ greeting }) => {
+const ItemListContainer = ({ greeting }) => {
   
     return (
-    <div>
-        <h1>{greeting}</h1>
-        </div>
+    <div> <h1>{ greeting }</h1></div>
     )
-}
+};
 
 export default ItemListContainer 
